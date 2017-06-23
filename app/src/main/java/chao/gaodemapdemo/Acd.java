aeffc7d18ecbdb0cf73e9980d1b689d1d741f230
zqc120 <zqc120@gmail.com>
2017-06-23 11:55:45 +0800

    ..

--- app/src/main/java/chao/gaodemapdemo/Acd.java
+++ app/src/main/java/chao/gaodemapdemo/Acd.java
@@ -1,11 +1,16 @@
 package chao.gaodemapdemo;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 /**
  * Created by Chao on 2017/6/22.
  */
 
 public class Acd extends Activity{
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
